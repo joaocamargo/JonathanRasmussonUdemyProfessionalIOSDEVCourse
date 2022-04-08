343374fd68e46f2106ff124ec80b63ad328b7745
joaocamargo <joao.camargo@gmail.com>
2022-04-08 15:45:10 -0300

    colocando o que ele nao colocou de titulo e subtitulo ... mudanca em constraints

--- Bankey/Bankey/Login/LoginViewController.swift
+++ Bankey/Bankey/Login/LoginViewController.swift
@@ -12,6 +12,8 @@ class LoginViewController: UIViewController {
     let loginView = LoginView()
     let signInButton = UIButton(type: .system)
     let errorMessageLabel = UILabel()
+    let titleApp = UILabel()
+    let subtitle = UILabel()
     
     var username: String? {
         return loginView.usernameTextField.text
@@ -24,6 +26,8 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        titleApp.text = "Bankey"
+        subtitle.text = "Your Premium source for all things banking!"
         style()
         layout()
     }
@@ -33,6 +37,13 @@ class LoginViewController: UIViewController {
 extension LoginViewController {
     private func style() {
         loginView.translatesAutoresizingMaskIntoConstraints = false
+        titleApp.translatesAutoresizingMaskIntoConstraints = false
+        subtitle.translatesAutoresizingMaskIntoConstraints = false
+
+        titleApp.font = UIFont.systemFont(ofSize: 36)
+        subtitle.font = UIFont.systemFont(ofSize: 22)
+        subtitle.numberOfLines = 0
+        subtitle.textAlignment = .center
         
         signInButton.translatesAutoresizingMaskIntoConstraints = false
         signInButton.configuration = .filled()
@@ -52,26 +63,31 @@ extension LoginViewController {
     
     
     private func layout() {
+        view.addSubview(titleApp)
         view.addSubview(loginView)
         view.addSubview(signInButton)
         view.addSubview(errorMessageLabel)
-        
+        view.addSubview(subtitle)
         
         NSLayoutConstraint.activate([
+            
+            titleApp.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            subtitle.bottomAnchor.constraint(equalTo: loginView.topAnchor, constant: -30),
+            titleApp.bottomAnchor.constraint(equalTo: subtitle.topAnchor, constant: -30),
+            subtitle.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            subtitle.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
             loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
-            //loginView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8)
             view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1),
             
-            
-            //signInButton.topAnchor.constraint(equalTo: loginView.bottomAnchor, constant: 16),
             signInButton.topAnchor.constraint(equalToSystemSpacingBelow: loginView.bottomAnchor, multiplier: 2),
             signInButton.leadingAnchor.constraint(equalTo: loginView.leadingAnchor),
             signInButton.trailingAnchor.constraint(equalTo: loginView.trailingAnchor),
-            
+//
             errorMessageLabel.topAnchor.constraint(equalToSystemSpacingBelow: signInButton.bottomAnchor, multiplier: 2),
             errorMessageLabel.leadingAnchor.constraint(equalTo: signInButton.leadingAnchor),
             errorMessageLabel.trailingAnchor.constraint(equalTo: signInButton.trailingAnchor),
+            
             
             
         ])
